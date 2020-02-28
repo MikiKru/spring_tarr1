457bd151192faa0d9671d61251c9f58cef935027
MikiKru <michal_kruczkowski@o2.pl>
2020-02-28 19:35:42 +0100

    spring v0

---
+++ src/main/java/pl/tarr1/spring_app/controller/HomeController.java
@@ -0,0 +1,17 @@
+package pl.tarr1.spring_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Wszystkie klasy z adnotacją @Controller lub @RestConttroller słóżą do mapowania żądań http
+// @Controller jest podstawowym stereotypem springa - beanem - zarządzana przez Spring Context
+// posiada automatyczne akcje nasłuchujące na określone żądania
+@Controller
+public class HomeController {
+
+    @GetMapping("/")        // mapuje na adres domowy "/" metodą GET -> czyli po dopasowaniu adres wywołuje metodę poniżej
+    public String home(){
+        return "home";       // w return zwracamy nazwę szablonu html (Thymeleaf) - bez .html
+    }
+
+}
