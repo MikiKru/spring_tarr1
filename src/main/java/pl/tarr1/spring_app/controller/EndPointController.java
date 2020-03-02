3a5be73c7bf72687786bb49000d6ab129c7d8baa
MikiKru <michal_kruczkowski@o2.pl>
2020-03-02 17:59:12 +0100

    sprign rest

---
+++ src/main/java/pl/tarr1/spring_app/controller/EndPointController.java
@@ -0,0 +1,8 @@
+package pl.tarr1.spring_app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+                    // publikuje REST API -> Representative State Transfer
+@RestController     // mapowanie żądań prokołu html -> GET, POST, PUT, DELETE
+public class EndPointController {
+
+}
