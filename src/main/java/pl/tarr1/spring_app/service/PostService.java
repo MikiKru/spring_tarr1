9a2d90f0a41e0e336aefd455f27a0dd660fc0816
MikiKru <michal_kruczkowski@o2.pl>
2020-03-04 17:56:55 +0100

    sprign rest

--- src/main/java/pl/tarr1/spring_app/service/PostService.java
+++ src/main/java/pl/tarr1/spring_app/service/PostService.java
@@ -2,8 +2,12 @@ package pl.tarr1.spring_app.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.tarr1.spring_app.model.Post;
+import pl.tarr1.spring_app.model.enums.Category;
 import pl.tarr1.spring_app.repository.PostRepository;
 import pl.tarr1.spring_app.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -15,11 +19,20 @@ public class PostService {
         this.userRepository = userRepository;
     }
     // 1. dodawanie posta przez użytkownika
-
+    public Post addPostByUser(String title, String content, Category category){
+        return null;
+    }
     // 2. wyszukiwanie posta po kluczu głównym
-
+    public Optional<Post> findPostById(Long postId){
+        return null;
+    }
     // 3. usuwanie posta
-
+    public boolean deletePostById(Long postId){
+        return false;
+    }
     // 4. zmiana danych posta
-
+    public Post updatePost(Long postId, String title, String content, Category category){
+        return null;
+    }
 }
+
