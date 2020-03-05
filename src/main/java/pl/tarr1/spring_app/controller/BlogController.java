0429f1d9ba11909835753a639a27d8ce5803b173
MikiKru <michal_kruczkowski@o2.pl>
2020-03-05 20:09:18 +0100

    spring + thymeleaf

--- src/main/java/pl/tarr1/spring_app/controller/BlogController.java
+++ src/main/java/pl/tarr1/spring_app/controller/BlogController.java
@@ -1,4 +1,13 @@
 package pl.tarr1.spring_app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlogController {
+
+    @GetMapping("/")
+    public String getPosts(){
+        return "posts";         // nazwa widoku bez .html
+    }
 }
