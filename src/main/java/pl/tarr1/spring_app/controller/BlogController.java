ae7449ef7255ca6527dd80c57bacc77c1e9e0756
MikiKru <michal_kruczkowski@o2.pl>
2020-03-06 19:47:23 +0100

    spring + thymeleaf

--- src/main/java/pl/tarr1/spring_app/controller/BlogController.java
+++ src/main/java/pl/tarr1/spring_app/controller/BlogController.java
@@ -1,13 +1,33 @@
 package pl.tarr1.spring_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.tarr1.spring_app.service.PostService;
+import pl.tarr1.spring_app.service.UserService;
 
 @Controller
 public class BlogController {
+    private PostService postService;
+    private UserService userService;
+    @Autowired
+    public BlogController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
 
     @GetMapping("/")
-    public String getPosts(){
+    public String getPosts(Model model){
+        // Model model - obiekt do komunikacji front-end i back-end
+        // model.addAttribute("nazwaObiektuNaFroncie", obiektJava);
+        model.addAttribute("posts", postService.getAllPostsOrderBySubmissionDateDesc());
         return "posts";         // nazwa widoku bez .html
+    }
+    @GetMapping("/posts&{postId}")
+    public String getPostById(@PathVariable("postId") Long postId, Model model){
+        model.addAttribute("post", postService.findPostById(postId));
+        return "post";
     }
 }
