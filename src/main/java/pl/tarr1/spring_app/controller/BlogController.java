29204429c74dd55a6433f3bbb84ccf039f41ca69
MikiKru <michal_kruczkowski@o2.pl>
2020-03-09 18:15:13 +0100

    spring + thymeleaf

--- src/main/java/pl/tarr1/spring_app/controller/BlogController.java
+++ src/main/java/pl/tarr1/spring_app/controller/BlogController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import pl.tarr1.spring_app.model.Post;
+import pl.tarr1.spring_app.model.enums.Category;
 import pl.tarr1.spring_app.service.PostService;
 import pl.tarr1.spring_app.service.UserService;
 
@@ -30,8 +32,10 @@ public class BlogController {
         model.addAttribute("post", postService.findPostById(postId));
         return "post";
     }
-    @GetMapping("/addPost")
-    public String addPost(){
+    @GetMapping("/addPost")                                       // wywołanie formularza i przekazanie parametrów POST /addPost
+    public String addPost(Model model){
+        model.addAttribute("post", new Post());                 // przekazanie obiektu post do th:object formularza
+        model.addAttribute("categories", Category.values());    // przekazanie tablicy kategorii do option formularza
         return "addPost";
     }
     @GetMapping("/register")
