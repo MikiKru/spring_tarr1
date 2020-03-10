3512dbae9028bec16fb43856ff35a8003a5b2627
MikiKru <michal_kruczkowski@o2.pl>
2020-03-10 19:48:12 +0100

    spring + thymeleaf

--- src/main/java/pl/tarr1/spring_app/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/tarr1/spring_app/configuration/WebSecurityConfiguration.java
@@ -9,9 +9,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     // metoda zabezpieczająca wykonywanie żądań protokołu http
-    protected void configure(HttpSecurity httpSecurity){
-        
-
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()       // autoryzuj poniższe żądania -> wskazuje które żądania wymagają określonych upranień
+                    .antMatchers("/addPost").hasAnyAuthority("ROLE_USER")
+                    .antMatchers("/posts&**").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+                    .antMatchers("/logout").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .anyRequest()              // każde żądanie nie wymienione pod autorizeRequests()
+                    .permitAll()           // nie wymaga zalogowania
+                .and()
+                    .csrf().disable()      // wyłączenie mechanizmu csrf
+                .formLogin()               // konfiguracja dedykowanego w aplikacji formularza logowania
+                    .loginPage("/login")   // konfiguracja mapingu formularza logowania
+                    .usernameParameter("email")     // konfiguracja nazwy pola name w formularzu login.html
+                    .passwordParameter("password")  // konfiguracja nazwy pola name w formularzu login.html
+                    .loginProcessingUrl("/login_process")   // adres gdzie przekazujemy dane logowania -> nie ma go w mappingu w kontrolerze
+                    .defaultSuccessUrl("/")                 // przekierowanie po poprawnym zalogowaniu metodą GET
+                    .failureUrl("/login_error")             // przekierowanie po błędnym logowaniu metodą GET
+                .and()
+                    .logout()               // metoda wylogowująca usera -> czyszcząca dane logowania (obiekt Authentication)
+                        .logoutUrl("/logout")               // adres do wylogowania (GET mapping) -> nie występuje w controllerze
+                        .logoutSuccessUrl("/");             // przekierowanie po wylogowaniu
     }
 
     // metoda wykonująca zapytanie do bazy danych i uwierzytelniająca zalogowanego usera
